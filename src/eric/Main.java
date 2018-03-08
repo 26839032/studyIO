be54dfd9d29d7d4262489332a1ad92afc0ab0279
明 赵 <zhaoming.yssh@foxmail.com>
2018-03-08 21:25:15 +0800

    Initial commit

---
+++ src/eric/Main.java
@@ -0,0 +1,23 @@
+package eric;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Main a =new Main();
+        System.out.printf(Integer.toString(a.add(3,5)));
+
+    }
+
+    public  void abc()
+    {
+        System.out.printf("this is a function");
+    }
+
+    public  int add(int x, int y)
+    {
+        return x+y;
+    }
+}
