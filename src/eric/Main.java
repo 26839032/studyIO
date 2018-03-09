3170f268d85369b4c736f3f7166101dea18b071a
user <26839032@qq.com>
2018-03-09 16:52:49 +0800

    Just for test

--- src/eric/Main.java
+++ src/eric/Main.java
@@ -5,19 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-
-        Main a =new Main();
-        System.out.print(Integer.toString(a.add(3, 5)));
+        Helper a = new Helper();
+        System.out.print(a.getName());
+        //System.out.print(Integer.toString(a.add(3, 5)));
 
     }
 
-    public  void abc()
-    {
-        System.out.print("this is a function");
-    }
 
-    private int add(int x, int y)
-    {
-        return x+y;
-    }
 }
